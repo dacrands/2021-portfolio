b6e40e40a82d64b78f05da263d20e7fd625d0903
David Crandall <davecrands@gmail.com>
2021-01-15 20:44:47 -0600

    Add navbar class

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
 }
 
 function Nav() {
-  return <div>
+  return <nav className={"navbar"}>
     <a href="">Home</a>
     <a href="">About</a>
     <a href="">Portfolio</a>
     <a href="">Contact</a>
-  </div>
+  </nav>
 }
