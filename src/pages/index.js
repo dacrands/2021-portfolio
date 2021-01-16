ec9fdb101a0e04003d22cf6282ed4b171022c23a
David Crandall <davecrands@gmail.com>
2021-01-15 20:18:44 -0600

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import "./main.css"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <main className={"landing"}>
+    <h1>David Crandall</h1>
+    <p>Software Engineer</p>
+    <Nav />
+  </main>
+}
+
+function Nav() {
+  return <div>
+    <a href="">Home</a>
+    <a href="">About</a>
+    <a href="">Portfolio</a>
+    <a href="">Contact</a>
+  </div>
 }
